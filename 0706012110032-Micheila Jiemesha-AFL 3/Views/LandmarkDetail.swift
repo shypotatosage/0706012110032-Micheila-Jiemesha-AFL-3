5d303528bb44dac8a8a3ad471e07732b7826bceb
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-11 10:06:37 +0700

    Chapter 1 (2) - Section 6

---
+++ 0706012110032-Micheila Jiemesha-AFL 3/Views/LandmarkDetail.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkDetail.swift
+//  0706012110032-Micheila Jiemesha-AFL 3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
