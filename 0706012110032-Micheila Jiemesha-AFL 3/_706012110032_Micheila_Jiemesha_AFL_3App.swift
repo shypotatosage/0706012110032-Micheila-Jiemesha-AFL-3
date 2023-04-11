f150360a658512935efcb2d8056b6d1082ca6bf8
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-11 09:37:43 +0700

    Initial Commit

---
+++ 0706012110032-Micheila Jiemesha-AFL 3/_706012110032_Micheila_Jiemesha_AFL_3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110032_Micheila_Jiemesha_AFL_3App.swift
+//  0706012110032-Micheila Jiemesha-AFL 3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110032_Micheila_Jiemesha_AFL_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
