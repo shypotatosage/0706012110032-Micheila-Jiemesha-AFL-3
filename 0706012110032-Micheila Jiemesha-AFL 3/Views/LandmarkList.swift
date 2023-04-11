fc18b0ece21adb1b2772dd40f1a9b5d650ff584b
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-11 10:02:57 +0700

    Chapter 1 (2) - Section 4

---
+++ 0706012110032-Micheila Jiemesha-AFL 3/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  0706012110032-Micheila Jiemesha-AFL 3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
