b4b01e546b54e7a8efcbb36fc2df8b681458fc85
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-11 14:53:26 +0700

    Chapter 1 (3) - Section 5

--- 0706012110032-Micheila Jiemesha-AFL 3/_706012110032_Micheila_Jiemesha_AFL_3App.swift
+++ 0706012110032-Micheila Jiemesha-AFL 3/_706012110032_Micheila_Jiemesha_AFL_3App.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct _706012110032_Micheila_Jiemesha_AFL_3App: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
