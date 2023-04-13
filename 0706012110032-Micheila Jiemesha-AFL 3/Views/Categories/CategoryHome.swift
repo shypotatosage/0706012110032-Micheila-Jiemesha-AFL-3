04c58256bc03ace94cfce8597bd7e9a0e96da76c
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-13 22:02:38 +0700

    Chapter 3 (1) - Section 1

---
+++ 0706012110032-Micheila Jiemesha-AFL 3/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  0706012110032-Micheila Jiemesha-AFL 3
+//
+//  Created by MacBook Pro on 13/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
