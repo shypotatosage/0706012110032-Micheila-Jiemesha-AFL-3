79e1e745962dd4ea6767293dc2a3d5c739e0a779
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-13 19:08:13 +0700

    Chapter 1 (3) - Section 6

---
+++ 0706012110032-Micheila Jiemesha-AFL 3/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  0706012110032-Micheila Jiemesha-AFL 3
+//
+//  Created by MacBook Pro on 13/04/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
