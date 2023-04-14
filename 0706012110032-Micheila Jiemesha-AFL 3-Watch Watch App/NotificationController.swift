b960f1b6735a44400ad5881b842a50c532e84d50
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-14 08:33:55 +0700

    Chapter 4 (2) - Section 1

---
+++ 0706012110032-Micheila Jiemesha-AFL 3-Watch Watch App/NotificationController.swift
@@ -0,0 +1,33 @@
+//
+//  NotificationController.swift
+//  0706012110032-Micheila Jiemesha-AFL 3-Watch Watch App
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+
+    override var body: NotificationView {
+        return NotificationView()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
---
+++ 0706012110032-Micheila Jiemesha-AFL 3-Watch Watch App/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  0706012110032-Micheila Jiemesha-AFL 3-Watch Watch App
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
